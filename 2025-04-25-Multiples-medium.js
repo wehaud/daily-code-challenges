41e63bf62c29c255970bb72ac8a602a24f277853
Anastasia Novoseltseva <chenastya@inbox.ru>
2025-04-28 22:15:30 +0300

    Update 2025-04-25-Multiples-medium.js

--- 2025-04-25-Multiples-medium.js
+++ 2025-04-25-Multiples-medium.js
@@ -1,6 +1,6 @@
 // Task: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0. Note: If the number is a multiple of both 3 and 5, only count it once.
 // Difficulty: medium
-// Solved on: 28.04.2025
+// Solved on: 25.04.2025
 // Feelings: Сперва забыла обработать двойное попадание 15, но все получилось!
 
 function solution(number){
